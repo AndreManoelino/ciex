1ab4a75335a280e65813e9c8e45950e3ddfef057
andreMAnoelino <agmphandre@gmail.com>
2025-08-15 15:29:18 -0300

    Alteraçoes realizadas de inserção de nomes de unidades no estados

--- app/Controllers/Clientes.php
+++ app/Controllers/Clientes.php
@@ -80,23 +80,16 @@ class Clientes extends Controller
         if (usuarioEhAdmin()) {
             $todasUnidades = [
                 'Minas Gerais' => [
-                    'Barreiro','Betim','Contagem','Contagem Avançada','Curvelo',
-                    'Governador Valadares','Ipatinga','Juiz de Fora','Montes Claros',
-                    'Poços de Caldas','Pouso Alegre','Praça Sete','São Sebastião do Paraiso',
-                    'Sete Lagoas','Sete Lagoas Avançada','Teofilo Otoni','Uberlândia',
-                    'Uberlândia Avançada','Varginha',
-                ],
-                'São Paulo' => [
-                    'Poupatempo Sé', 'Poupatempo Santo Amaro', 'Poupatempo Itaquera',
-                    'Poupatempo Luz', 'Poupatempo Móvel (Cidade Tiradentes)',
-                    'Poupatempo Móvel (Ipiranga)', 'Poupatempo Guarulhos', 'Poupatempo Campinas'
-                ],
-                'Rio de Janeiro' => [
-                    'Poupa Tempo Recreio dos Bandeirantes', 'Poupa Tempo Zona Oeste',
-                    'Poupa Tempo Baixada', 'Poupa Tempo São Gonçalo', 'Poupa Tempo Bangu'
-                ],
-                'Ceara' => ['Unidade 1', 'Unidade 2', 'Unidade 3'],
-                'Parana' => ['Unidade A', 'Unidade B', 'Unidade C'],
+                'Barreiro','Betim','Contagem','Contagem Avançada','Curvelo',
+                'Governador Valadares','Ipatinga','Juiz de Fora','Montes Claros',
+                'Poços de Caldas','Pouso Alegre','Praça Sete','Regional BH','São Sebastião do Paraiso',
+                'Sete Lagoas','Sete Lagoas Avançada','Teofilo Otoni','Uberlândia',
+                'Uberlândia Avançada','Varginha'
+            ],
+            'São Paulo' => ['Administração Regional','Avaré','Botucatu','Capão Bonito','Caraguatatuba','Guaratingueta','Guarujá','Iguape','Itapeva','Itaquaquecetuba','Itu','Jacareí','Mogi das Cruzes','Pindamonhangaba','Piquete','Praia Grande','Registro','Santos','São José dos Campos','São Vicente','Sorocaba','Tatuí','Taubaté'],
+            'Rio de Janeiro' => ['Bangu', 'Caxias'],
+            'Ceara' => ['Antonio Bezerra', 'Central Administrativa','Centro Fortaleza','Juazeiro do Norte','Mesejana','Papicu','Parangaba','Sobral'],
+            'Parana' => ['Administração Central','Apucarama','Arapongas','Araucaria','Campo Largo','Cascavel','Colombo','Curitiba - Boa Vista','Curitiba - Centro','Curitiba - Pinheirinho','Foz do Iguaçu','Guarapuava','Londrina','Maringa','Paranagua','Pinhais','Ponta Grossa','São José dos Pinhais','Toledo','UDS','Umurama'],
             ];
         }
 
